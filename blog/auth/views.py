6220ca9e107ca83710bce78fcdf341806b728e02
KosarevPV <pavel999kosarev@gmail.com>
2023-04-29 20:21:58 +0300

    lesson_5

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, login_user, logout_user, login_required
-from werkzeug.security import check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.exc import IntegrityError
 
+from blog.models.database import db
+from blog.forms.user import RegistrationForm, LoginForm
 from blog.models import User
 
 auth = Blueprint("auth", __name__)
@@ -22,20 +25,58 @@ def unauthorized():
 
 @auth.route("/login/", methods=["GET", "POST"], endpoint="login")
 def login():
-    if request.method == "GET":
-        return render_template("auth/login.html")
+    if current_user.is_authenticated:
+        return redirect(url_for("post.post_list"))
 
-    username = request.form.get("username")
-    password = request.form.get("password")
-    user = User.query.filter_by(username=username).first()
+    error = None
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Login success')
+            return redirect(url_for("post.post_list"))
 
-    if not username or not check_password_hash(user.password, password):
-        flash('Check your login details')
-        return render_template("auth/login.html", error="username not passed")
+        form.email.errors.append("Check your login details!")
+        form.password.errors.append("Check your login details!")
+        return render_template("auth/login.html", form=form)
 
-    login_user(user)
-    flash('Login success')
-    return redirect(url_for("post.post_list"))
+    return render_template("auth/login.html", form=form, error=error)
+
+
+@auth.route("/register/", methods=["GET", "POST"], endpoint="register")
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("post.post_list"))
+    error = None
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        if User.query.filter_by(username=form.username.data).count():
+            form.username.errors.append("username already exists!")
+            return render_template("auth/register.html", form=form)
+        if User.query.filter_by(email=form.email.data).count():
+            form.email.errors.append("email already exists!")
+            return render_template("auth/register.html", form=form)
+
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            username=form.username.data,
+            email=form.email.data,
+            is_staff=False,
+        )
+        user.password = generate_password_hash(form.password.data)
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception("Could not create user!")
+            error = "Could not create user!"
+        else:
+            current_app.logger.info("Created user %s", user)
+            login_user(user)
+            return redirect(url_for("post.post_list"))
+    return render_template("auth/register.html", form=form, error=error)
 
 
 @auth.route("/logout/", endpoint="logout")
